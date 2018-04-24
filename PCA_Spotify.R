ccee51f94b8d36f61e18e43abdb8a945a544fceb
suhitd1729 <32601049+suhitd1729@users.noreply.github.com>
2018-04-24 01:45:26 -0400

    modified the R code added Clustering code

--- PCA_Spotify.R
+++ PCA_Spotify.R
@@ -1,6 +1,14 @@
-setwd("~/Documents/GitHub/Spotty_Tagger")
+rm(list=ls())
+library(ggplot2)
+library("multtest")
+library("fpc")
+library("cluster")
+#setwd("~/Documents/GitHub/Spotty_Tagger")
+setwd("C:/DragonBallZ/git_Repo/Spotty_Tagger")
 
-tracks = read.csv('billboardTop100.csv')
+tracks = read.csv('tracksMusicInfo.csv')
+
+summary(tracks)
 
 colsList = c('acousticness','danceability', 'duration_ms', 'energy', 'instrumentalness', 'key','liveness', 'loudness', 'mode', 'speechiness', 'tempo','time_signature', 'valence')
 
@@ -9,6 +17,10 @@ tracksColFil = tracks[,colsList]
 trackPCA = prcomp(tracksColFil)
 pc1 = trackPCA$x[,1]
 pc2 = trackPCA$x[,2]
+pc3 = trackPCA$x[,3]
+pc4 = trackPCA$x[,4]
+pc5 = trackPCA$x[,5]
+
 artist = as.character(tracks[,'track_name'])
 artist = as.character(tracks[,'artist'])
 
@@ -20,11 +32,32 @@ with(df, text(pc1~pc2, labels = artist), pos = 4)
 text(pc1 ~ pc2, labels=artist, cex= 0.7)
 
 
-ggplot(df, aes(pc1,pc2)) + geom_point() + geom_text(aes(label=artist))
+ggplot(df, aes(pc4,pc5,color=tracks$genre)) + geom_point() #+ geom_text(aes(label=artist))
 
 
-scaledDF = scale(tracksColFil)
+#####################################
+# biplot 
+tracksNumeric <- tracks[,c(6:18)]  
+pc_sdata <- prcomp(tracksColFil, center = TRUE, scale = TRUE)
+biplot(pc_sdata)
+#####################################
 
+
+d<- dist(tracksColFil)
+
+dim(as.matrix(d))
+# avg linkage
+
+hc_avg <- hclust(d, method = "average")
+x11()
+plot(hc_avg, main="Average Linkage",labels = tracks$genre)
+
+data2clusters <- cutree(hc_avg,9)
+
+hclustResultDF <- data.frame(tracks$artist,tracks$track_name,tracks$album,tracks$genre,data2clusters)
+
+# 3395 for 1 
+sum(hclustResultDF$data2clusters == 1)
 
 trackPCA = prcomp(scaledDF)
 pc1 = trackPCA$x[,1]
@@ -37,8 +70,24 @@ with(df, text(pc1~pc2, labels = artist), pos = 4)
 
 
 text(pc1 ~ pc2, labels=artist, cex= 0.7)
-library(ggplot2)
+
 
 ggplot(df, aes(pc1,pc2)) + geom_point() + geom_text(aes(label=artist))
 
 summary(tracksColFil)
+
+####################################
+# KMediods
+####################################
+kmed <- pamk(tracksColFil)
+
+# let the program decide "optimal k"
+kmed$nc # getting this value as 3
+
+# tabulate the results
+table(kmed$pamobject$clustering, tracks$genre)
+
+x11()
+layout(matrix(c(1,2),1,2))
+plot(kmed$pamobject)
+
