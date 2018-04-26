1978a5ac1cf98d1db9762ef3e13ed90eb0858d5b
varad.s.tupe <varad.s.tupe@gmail.com>
2018-04-26 19:59:41 -0400

    Kmeans implementation

---
+++ PCA_Spotify_VST.R
@@ -0,0 +1,159 @@
+rm(list=ls())
+library(ggplot2)
+library("multtest")
+library("fpc")
+library("cluster")
+library("lsa")
+library("proxy")
+setwd("~/Documents/GitHub/Spotty_Tagger")
+#setwd("C:/DragonBallZ/git_Repo/Spotty_Tagger")
+
+tracks = read.csv('tracksMusicInfo.csv')
+tracks = tracks[which(tracks$genre == "Rock" | tracks$genre == "Pop"),]
+
+summary(tracks)
+
+colsList = c('acousticness','danceability', 'duration_ms', 'energy', 'instrumentalness', 'key','liveness', 'loudness', 'mode', 'speechiness', 'tempo','time_signature', 'valence')
+
+tracksColFil = tracks[,colsList]
+
+trackPCA = prcomp(tracksColFil)
+
+pc1 = trackPCA$x[,1]
+pc2 = trackPCA$x[,2]
+pc3 = trackPCA$x[,3]
+pc4 = trackPCA$x[,4]
+pc5 = trackPCA$x[,5]
+
+track_name = as.character(tracks[,'track_name'])
+artist = as.character(tracks[,'artist'])
+
+df = data.frame(artist,pc1,pc2)
+
+ggplot(df, aes(pc1,pc2,color=as.factor(kMeansTrk$cluster))) + geom_point() + geom_text(aes(label=tracks$genre))
+
+
+#####################################
+# biplot 
+tracksNumeric <- tracks[,c(6:18)]  
+pc_sdata <- prcomp(tracksColFil, center = TRUE, scale = TRUE)
+biplot(pc_sdata)
+#####################################
+
+
+d<- dist(tracksColFil)
+d = dist(tracksColFil,method = "Cosine")
+
+dim(as.matrix(d))
+# avg linkage
+
+hc_avg <- hclust(d, method = "average")
+x11()
+plot(hc_avg, main="Average Linkage",labels = tracks$genre)
+
+data2clusters <- cutree(hc_avg,9)
+
+hclustResultDF <- data.frame(tracks$artist,tracks$track_name,tracks$album,tracks$genre,data2clusters)
+
+# 3395 for 1 
+sum(hclustResultDF$data2clusters == 1)
+
+trackPCA = prcomp(scaledDF)
+pc1 = trackPCA$x[,1]
+pc2 = trackPCA$x[,2]
+artist = as.character(tracks[,'artist'])
+
+df = data.frame(artist,pc1,pc2)
+plot(df$pc1,df$pc2) 
+with(df, text(pc1~pc2, labels = artist), pos = 4)
+
+
+text(pc1 ~ pc2, labels=artist, cex= 0.7)
+
+
+ggplot(df, aes(pc1,pc2)) + geom_point() + geom_text(aes(label=artist))
+
+summary(tracksColFil)
+
+####################################
+# KMediods
+####################################
+kmed <- pamk(tracksColFil)
+
+# let the program decide "optimal k"
+kmed$nc # getting this value as 3
+
+# tabulate the results
+table(kmed$pamobject$clustering, tracks$genre)
+
+kmed9DF = data.frame(kmed$pamobject$clustering,tracks$artist,tracks$genre,tracks$album,tracks$track_name)
+
+x11()
+layout(matrix(c(1,2),1,2))
+plot(kmed$pamobject)
+
+#####
+# Kmeans
+#####
+
+kMeansTrk = kmeans(d,centers = 9)
+
+table(kMeansTrk$cluster,tracks$genre)
+
+
+
+########################################################
+# Using PCA
+trackPCADF = trackPCA$x
+
+d<- dist(trackPCADF )
+d = cosine(trackPCADF)
+
+
+hc_avg <- hclust(d, method = "average")
+x11()
+plot(hc_avg, main="Average Linkage",labels = tracks$genre)
+
+data2clusters <- cutree(hc_avg,9)
+table(tracks$genre,data2clusters)
+hclustResultDF <- data.frame(tracks$artist,tracks$track_name,tracks$album,tracks$genre,data2clusters)
+
+# 3395 for 1 
+sum(hclustResultDF$data2clusters == 1)
+
+trackPCA = prcomp(scaledDF)
+pc1 = trackPCA$x[,1]
+pc2 = trackPCA$x[,2]
+artist = as.character(tracks[,'artist'])
+
+df = data.frame(artist,pc1,pc2)
+plot(df$pc1,df$pc2) 
+with(df, text(pc1~pc2, labels = artist), pos = 4)
+
+
+text(pc1 ~ pc2, labels=artist, cex= 0.7)
+
+
+ggplot(df, aes(pc1,pc2)) + geom_point() + geom_text(aes(label=artist))
+
+summary(tracksColFil)
+
+####################################
+# KMediods
+####################################
+kmed <- pamk(tracksColFil)
+
+# let the program decide "optimal k"
+kmed$nc # getting this value as 3
+
+# tabulate the results
+table(kmed$pamobject$clustering, tracks$genre)
+
+kmed9DF = data.frame(kmed$pamobject$clustering,tracks$artist,tracks$genre,tracks$album,tracks$track_name)
+
+x11()
+layout(matrix(c(1,2),1,2))
+plot(kmed$pamobject)
+
+
+
