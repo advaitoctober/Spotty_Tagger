5646ab133d4acdca69e6d4cc423c8821db7f3d95
varad.s.tupe <varad.s.tupe@gmail.com>
2018-05-01 10:38:26 -0400

    Added Images and and updated code

--- PCA_Spotify_VST1000.R
+++ PCA_Spotify_VST1000.R
@@ -5,6 +5,10 @@ library("fpc")
 library("cluster")
 library("lsa")
 library("proxy")
+library("kmed")
+library("NbClust")
+library("factoextra")
+library("ggfortify")
 setwd("~/Documents/GitHub/Spotty_Tagger")
 #setwd("C:/DragonBallZ/git_Repo/Spotty_Tagger")
 
@@ -33,10 +37,10 @@ mood = as.character(tracks[,'mood'])
 df = data.frame(artist,pc1,pc2)
 
 ####
-d<- dist(tracksColFil)
+#d<- dist(tracksColFil)
 d = dist(tracksColFil,method = "Cosine")
-d = dist(tracksColFil,method = "canberra")
-d = dist(tracksColFil,method = "binary")
+#d = dist(tracksColFil,method = "canberra")
+#d = dist(tracksColFil,method = "binary")
 d
 dim(as.matrix(d))
 #####
@@ -131,11 +135,11 @@ track.measures <- c("acousticness","danceability","duration_ms","energy",
                     "instrumentalness","liveness","loudness","speechiness","tempo",
                     "valence","energy_level","positivity_level")
 
-data = data[complete.cases(data), ]
-data = unique(data)
+#data = data[complete.cases(data), ]
+#data = unique(data)
 
-data_Bk = data_Bk[complete.cases(data_Bk), ]
-data_Bk = unique(data_Bk)
+#data_Bk = data_Bk[complete.cases(data_Bk), ]
+#data_Bk = unique(data_Bk)
 nrow(data_Bk)
 
 nrow(data)
@@ -147,12 +151,12 @@ Labels <-data$genre
 colors  = rainbow(length(unique(Labels)))
 names(colors) = unique(Labels)
 
-data.scaled = data.scaled[complete.cases(data.scaled), ]
+#data.scaled = data.scaled[complete.cases(data.scaled), ]
 tsne <- Rtsne(data.scaled, dims = 5, perplexity=30, verbose=TRUE, max_iter = 2000,check_duplicates = FALSE)
 
 
 dfts = data.frame(pc1=tsne$Y[,1],pc2=tsne$Y[,2])
-ggplot(dfts, aes(pc1,pc2,color=as.factor(data_Bk$genre))) + geom_point() #+geom_text(aes(label=data_Bk$genre))
+ggplot(dfts, aes(pc1,pc2,color=as.factor(data_Bk$genre))) + geom_point() +geom_text(aes(label=data_Bk$genre))
 
 
 
@@ -201,3 +205,136 @@ tracks_CLust = tracks_CLust[,metaCol]
 
 
 ggplot(df, aes(pc1,pc2,color=as.factor(kMeansTrk$cluster))) + geom_point() #+ geom_text(aes(label=tracks$positivity_level))
+
+
+
+
+#################IMP CODE ###################
+tracks = read.csv('dataWithLabel1000.csv')
+colsList = c('acousticness','danceability', 'energy', 'instrumentalness','liveness', 'loudness', 'speechiness', 'tempo', 'valence','positivity_level','energy_level')
+
+tracksColFil = tracks[,colsList]
+
+#Transform data using tsne
+
+library("Rtsne")
+data <- tracksColFil
+data_Bk <- tracks
+track.measures <- c("acousticness","danceability","duration_ms","energy",
+                    "instrumentalness","liveness","loudness","speechiness","tempo",
+                    "valence","energy_level","positivity_level")
+
+#data = data[complete.cases(data), ]
+#data = unique(data)
+
+#data_Bk = data_Bk[complete.cases(data_Bk), ]
+#data_Bk = unique(data_Bk)
+nrow(data_Bk)
+
+nrow(data)
+data <- data[,track.measures]
+data.scaled <- data
+data.scaled <- scale(data)
+
+Labels <-data$genre
+colors  = rainbow(length(unique(Labels)))
+names(colors) = unique(Labels)
+
+#data.scaled = data.scaled[complete.cases(data.scaled), ]
+tsne <- Rtsne(data.scaled, dims = 5, perplexity=30, verbose=TRUE, max_iter = 2000,check_duplicates = FALSE)
+
+
+d = dist(tsne$Y)
+
+
+
+dim(as.matrix(tsne$Y))
+dim(as.matrix(d))
+
+
+dfts = data.frame(pc1=tsne$Y[,1],pc2=tsne$Y[,2])
+pc1=tsne$Y[,1]
+pc2=tsne$Y[,2]
+##
+
+#Finding our best number of cluster for Kmeans
+#Elbow Method for finding the optimal number of clusters
+set.seed(123)
+# Compute and plot wss for k = 2 to k = 15.
+k.max <- 10
+data <- d
+wss <- sapply(1:k.max, 
+              function(k){kmeans(data, k, nstart=10,iter.max = 10 )$tot.withinss})
+wss
+plot(1:k.max, wss,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
+####
+
+############# KMedoids
+set.seed(123)
+# Compute and plot wss for k = 2 to k = 15.
+k.max <- 10
+data <- d
+wss <- sapply(1:k.max, 
+              function(k){fastkmed(d, k, iterate = 10)$minimum_distance})
+wss
+plot(1:k.max, wss,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
+####
+
+nb <- NbClust(tsne$Y, distance = "euclidean", min.nc = 2,
+              max.nc = 10, method = "median")
+
+
+#####
+
+
+
+#Kmed
+kmedFast = fastkmed(d, 5, iterate = 10)
+pamKmed = pam(tsne$Y, 4, metric = "euclidean", stand = FALSE)
+round(kmedFast$minimum_distance)
+
+pamKmed
+
+#############
+
+#Best clust Kmeans
+kMeansTrk = kmeans(d,centers = 5,iter.max = 10)
+kMeansTrk$tot.withinss
+
+table(kMeansTrk$cluster,data_Bk$energy_level)
+table(kMeansTrk$cluster,data_Bk$positivity_level)
+
+tracks_CLust = tracks
+
+tracks_CLust$clust = kMeansTrk$cluster
+metaCol = c('album', 'artist', 'genre', 'track_name','clust')
+tracks_CLust = tracks_CLust[,metaCol]
+
+dfts = data.frame(pc1=tsne$Y[,1],pc2=tsne$Y[,2])
+
+#Plot for Kmeans
+kMeanClust = as.factor(kMeansTrk$cluster)
+ggplot(dfts, aes(pc1,pc2,color=kMeanClust)) + geom_point() +geom_text(aes(label=data_Bk$track_name))
+
+ggplot(dfts, aes(pc1,pc2,color=kMeanClust)) + geom_point() +geom_text(aes(label=data_Bk$genre))
+
+
+#Plot for KMed
+kMedoids_Cluster = as.factor(kmedFast$cluster)
+ggplot(dfts, aes(pc1,pc2,color=kMedoids_Cluster)) + geom_point() +geom_text(aes(label=data_Bk$track_name))
+ggplot(dfts, aes(pc1,pc2,color=kMedoids_Cluster)) + geom_point() +geom_text(aes(label=data_Bk$genre))
+
+
+#Plot for Pam Kmed
+ggplot(dfts, aes(pc1,pc2,color=as.factor(pamKmed$clustering))) + geom_point() +geom_text(aes(label=data_Bk$track_name))
+ggplot(dfts, aes(pc1,pc2,color=as.factor(pamKmed$clustering))) + geom_point() +geom_text(aes(label=data_Bk$genre))
+
+
+tracks_CLust$clust = kMeansTrk$cluster
+
