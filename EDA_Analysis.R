31cd9292909fab2bf4469f1f135fce4a489399ea
suhitd1729 <32601049+suhitd1729@users.noreply.github.com>
2018-05-01 18:46:40 -0400

    Correlation Plot

--- EDA_Analysis.R
+++ EDA_Analysis.R
@@ -8,12 +8,20 @@ require(gdata)
 
 data <- read.csv('dataWithLabel1000.csv')
 
-colsList = c('acousticness','danceability', 'energy', 'instrumentalness','liveness', 'loudness', 'speechiness', 'tempo', 'valence')
+
+colsList = c('acousticness','danceability', 'energy', 'instrumentalness','liveness', 'loudness', 'speechiness', 'tempo', 'valence','positivity_level' , 'energy_level')
 
 tracksColFil = data[,colsList]
+
+
+# filling NA values with 0 
+
+tracksColFil$energy_level <- ifelse(is.na(tracksColFil$energy_level), 0, tracksColFil$energy_level)
+tracksColFil$positivity_level <- ifelse(is.na(tracksColFil$positivity_level), 0, tracksColFil$positivity_level)
 
 pairs(tracksColFil)
 
 corrData <- cor(tracksColFil)
 corrplot(corrData, method = "circle")
 corrplot(corrData, method = "number") 
+
